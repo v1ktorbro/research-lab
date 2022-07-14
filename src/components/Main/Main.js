5b252bba0573edf847fb4f141b69420116eaca8f
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-14 23:42:56 +0500

    feat(Main): connect, create

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import './Main.scss';
+import React from 'react';
+import SignIn from '../SignIn/SignIn';
+
+function Main() {
+  return (
+    <main className='main'>
+      <p>main here</p>
+      <SignIn />
+    </main>
+  );
+}
+
+export default Main;
