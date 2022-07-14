6ea3cef39f6d8e5f967f75260f672b6d2289c8b7
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-14 23:42:14 +0500

    feat(App): connect Main component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Main from '../Main/Main';
+
 function App() {
   return (
-    <div className="App">
-    <h1>Hello, world!</h1>
-    </div>
+    <>
+      <Main />
+    </>
   );
 }
 
