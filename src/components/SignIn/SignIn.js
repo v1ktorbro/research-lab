1725fc7277e893d1fcf8cd2a11a60c1a9431a824
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-15 16:27:04 +0500

    fix(SignIn): style how show in figma

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -11,7 +11,16 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#414549',
+    },
+    custom: {
+      main: '#0298A9',
+    }
+  },
+});
 
 export default function SignIn() {
   const handleSubmit = (event) => {
@@ -25,7 +34,7 @@ export default function SignIn() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" sx={{textAlign: 'start'}} maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
@@ -35,7 +44,7 @@ export default function SignIn() {
             alignItems: 'center',
           }}
         >
-          <Typography component="h1" variant="h5">
+          <Typography component="h1" variant="h5" sx={{alignSelf: 'flex-start'}}>
             Sign in
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
@@ -59,24 +68,27 @@ export default function SignIn() {
               id="password"
               autoComplete="current-password"
             />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Grid container >
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link href="#" variant="body2" sx={{textDecoration: 'none', color: 'rgba(0, 0, 0, .7)'}}>
                   Forgot password?
                 </Link>
               </Grid>
+            </Grid>
+            <Grid container sx={{flexDirection: 'column'}}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color='custom'
+                sx={{ mt: 3, mb: 2, width: '160px' }}
+              >
+                Sign In
+              </Button>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="custom" />}
+                label="Remember me"
+              />
             </Grid>
           </Box>
         </Box>
