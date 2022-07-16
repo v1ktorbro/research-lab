cdb1e45ca87965014c95c4a135792383e020a567
Viktor Abrosimov <vitya.abrosimoff2016@ya.ru>
2022-07-16 21:07:53 +0500

    fix(BitButton): correct display of the disabled attribute dependingon the authState

--- src/components/BigButton/BigButton.js
+++ src/components/BigButton/BigButton.js
@@ -1,10 +1,12 @@
 import './BigButton.scss';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 export default function BigButton({nameBtn, hexColor}) {
+  const authState = useSelector((state) => state.auth.isAuth);
   return (
-    <button disabled id={nameBtn} className={`big-button big-button_${nameBtn}`} style={{backgroundColor: hexColor}}>
+    <button disabled={authState ? false : true} id={nameBtn} className={`big-button big-button_${nameBtn}`} style={{backgroundColor: hexColor}}>
       <span className={`big-button-text big-button-text_${nameBtn}`} >{nameBtn.toUpperCase()}</span>
     </button>
   );
