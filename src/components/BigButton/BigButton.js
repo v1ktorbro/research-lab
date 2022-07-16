ca98051a69089faed88b7f0b5da357898101c09c
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-16 13:58:30 +0500

    refactor(BigButton): rename class one letter

--- src/components/BigButton/BigButton.js
+++ src/components/BigButton/BigButton.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 
 export default function BigButton({nameBtn, hexColor}) {
   return (
-    <button id={nameBtn} className={`bit-button bit-button_${nameBtn}`} style={{backgroundColor: hexColor}}>
-      <span className={`bit-button-text bit-button-text_${nameBtn}`} >{nameBtn.toUpperCase()}</span>
+    <button disabled id={nameBtn} className={`big-button big-button_${nameBtn}`} style={{backgroundColor: hexColor}}>
+      <span className={`big-button-text big-button-text_${nameBtn}`} >{nameBtn.toUpperCase()}</span>
     </button>
   );
 }
