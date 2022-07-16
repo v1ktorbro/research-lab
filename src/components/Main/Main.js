b78601dd377c45b71ec770ab123e9f2624a057ae
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-16 14:01:58 +0500

    refactor(Main): add container to sign-in form and greeting text

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,16 @@
 import './Main.scss';
 import React from 'react';
 import SignIn from '../SignIn/SignIn';
+import BtnsContainer from '../BtnsContainer/BtnsContainer';
 
 function Main() {
   return (
     <main className='main'>
-      <p>Welcome to the ...</p>
-      <SignIn />
+      <BtnsContainer />
+      <div className='main__sign-in-block'>
+        <p className='main__greeting-text'>Welcome to the ...</p>
+        <SignIn />
+      </div>
     </main>
   );
 }
