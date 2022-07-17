54b59152dbba957367a74c92c3c3ec8061b9ad70
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-17 13:27:55 +0500

    feat(index.js): connect BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
@@ -8,8 +9,10 @@ import { store } from './store/index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
