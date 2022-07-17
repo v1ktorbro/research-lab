3a6ff94aefb944a83c623d58f69b860545d9436e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-17 15:47:15 +0500

    feat(PageOfUser): create, layout, connect

---
+++ src/components/PageOfUser/PageOfUser.js
@@ -0,0 +1,31 @@
+import './PageOfUser.scss';
+import React from 'react';
+import defaultAvatar from '../../images/default_avatar.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setLoginDataUser } from '../../store/authReducer';
+
+function PageOfUser() {
+  const currentEmailUser = useSelector((state) => state.auth.email);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handlerBtnLogout = () => {
+      dispatch(setLoginDataUser({email: '', isAuth: false}));
+      navigate('/');
+      localStorage.removeItem('loginDataUser');
+  };
+
+  return (
+    <article className='page-of-user'>
+      <p className='page-of-user__greeting-text'>Welcome to the finished test task from Viktor Abrosimov</p>
+      <div className='page-of-user__block-info'>
+        <span className='page-of-user__avatar' src={defaultAvatar} alt='avatar' />
+        <h4 className='page-of-user__email'>{currentEmailUser}</h4>
+        <button onClick={handlerBtnLogout} className='page-of-user__btn-logout'>Logout</button>
+      </div>
+    </article>
+  );
+}
+
+export default PageOfUser;
