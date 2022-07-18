7326024422d9a9447d625941819c82caa914450e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-18 17:37:01 +0500

    refactor(BtnsContainer): semantic changes

--- src/components/BtnsContainer/BtnsContainer.js
+++ src/components/BtnsContainer/BtnsContainer.js
@@ -5,9 +5,9 @@ import BigButton from '../BigButton/BigButton';
 export default function BtnsContainer() {
   return (
     <article className='btns-container'>
-      <BigButton nameBtn='projects' hexColor='#414549' />
-      <BigButton nameBtn='market' hexColor='#86BD24' />
-      <BigButton nameBtn='user' hexColor='#0298A9' />
+      <BigButton nameBtn='projects' />
+      <BigButton nameBtn='market' />
+      <BigButton nameBtn='user' />
     </article>
   );
 }
